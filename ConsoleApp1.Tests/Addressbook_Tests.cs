03d3351c4e74a320d096ad0e511d70f610df062c
corneliala <larzo@LAPTOP-0FQ835PP>
2023-02-01 16:00:48 +0100

    Add project files.

---
+++ ConsoleApp1.Tests/Addressbook_Tests.cs
@@ -0,0 +1,28 @@
+﻿
+
+using ConsoleApp1.Models;
+using ConsoleApp1.Services;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass]
+    public class Addressbook_Tests
+    {
+        [TestMethod]
+        public void Should_Add_Contact_To_List()
+        {
+            //arrange
+            Addressbook addressbook = new Addressbook();
+            Contact contact = new Contact();
+            
+
+
+            //act
+
+            addressbook.ContactList.Add(contact);
+
+            //assert
+            Assert.AreEqual(1, addressbook.ContactList.Count);
+        }
+    }
+}
---
+++ ConsoleApp1/Services/Addressbook.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Services
+{
+    public class Addressbook
+    {
+        public List<Contact> ContactList { get; set; } = new List<Contact>();
+    }
+}
